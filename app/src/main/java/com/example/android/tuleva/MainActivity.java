1d382d67cce8a99f4ade7b57d7904bfee60c14ab
katknuuttila <katten.knut@gmail.com>
2019-04-20 12:14:36 +0200

    third commit

--- app/src/main/java/com/example/android/tuleva/MainActivity.java
+++ app/src/main/java/com/example/android/tuleva/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
+
 }
