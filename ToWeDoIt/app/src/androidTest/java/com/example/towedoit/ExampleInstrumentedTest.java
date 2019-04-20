688d764c859cc940bb518930d4c130cccdb1067d
katknuuttila <katten.knut@gmail.com>
2019-04-20 18:19:40 +0200

    redo commit

--- ToWeDoIt/app/src/androidTest/java/com/example/towedoit/ExampleInstrumentedTest.java
+++ ToWeDoIt/app/src/androidTest/java/com/example/towedoit/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
-package com.example.android.tuleva;
+package com.example.towedoit;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.android.tuleva", appContext.getPackageName());
+        assertEquals("com.example.towedoit", appContext.getPackageName());
     }
 }
